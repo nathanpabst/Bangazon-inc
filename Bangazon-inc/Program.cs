166753cda8683cddb1ceda129eeae53668c4a3ea
unknown <nathan.pabst@gmail.com>
2018-09-01 14:07:24 -0500

    wip

---
+++ Bangazon-inc/Program.cs
@@ -0,0 +1,44 @@
+﻿using Bangazon_inc.Stuff;
+using System;
+using System.Collections.Generic;
+
+namespace Bangazon_inc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public class Company
+        {
+            /*
+                Some readonly properties
+            */
+            public string Name { get; }
+            public DateTime CreatedOn { get; }
+            // Create a property for holding a list of current employees
+            List<Employee> employees = new List<Employee>();
+
+            // Create a method that allows external code to add an employee
+            public void AddEmployee(Employee newEmployee)
+            {
+                employees.Add(newEmployee);
+            }
+
+            // Create a method that allows external code to remove an employee
+            public void RemoveEmployee(Employee firedEmployee)
+            {
+                employees.Remove(firedEmployee);
+            }
+            /*
+                Create a constructor method that accepts two arguments:
+                    1. The name of the company
+                    2. The date it was created
+
+                The constructor will set the value of the public properties
+            */
+        }
+    }
+}
---
+++ Bangazon-inc/Stuff/Employee.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bangazon_inc.Stuff
+{
+    class Employee
+    {
+        public string EmployeeName { get; private set; } = "";
+
+        public string JobTitle { get; private set; } = "";
+
+        public int StartDate {get; private set;} = 0;
+
+        
+    }
+    
+}
